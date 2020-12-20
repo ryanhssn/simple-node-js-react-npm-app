9c83e682e919edd9670136b773bd1dddec97c60b
Bilal Hussain <hussain.bilal000786@gmail.com>
2020-12-21 01:06:03 +0400

    added enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+import Title from './Title';
+
+const title = 'Test Title';
+let wrapped = shallow(<Title>{title}</Title>);
+describe('Title', () => {
+  it('should render the Title Component correctly', () => {   
+    expect(wrapped).toMatchSnapshot();
+  });
+  it('renders the Titles children', () => { 
+    expect(wrapped.find('h1').text()).toEqual(title);
+  });
+});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
